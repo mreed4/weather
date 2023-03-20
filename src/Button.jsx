99b6fbc35d6c71d48da37dabad521d446394c4e6
Matthew <78713317+mreed4@users.noreply.github.com>
2023-03-20 15:00:08 -0700

    Clean up

--- src/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function Button() {
-  const [count, setCount] = useState(0);
-
-  return <button onClick={() => setCount(count + 1)}>Click me</button>;
-}
