740941f3527bd666b565b14bae6448664833ce7d
Matthew <78713317+mreed4@users.noreply.github.com>
2023-03-23 21:28:41 -0700

    Simplify

--- src/Input.jsx
+++ src/Input.jsx
@@ -4,9 +4,5 @@ import { AppContext } from "./AppContext";
 export default function Input() {
   const { location, handleOnChange } = useContext(AppContext);
 
-  return (
-    <div>
-      <input type="text" name="city-name" id="city-name" placeholder="City name" value={location} onChange={handleOnChange} />
-    </div>
-  );
+  return <input type="text" name="city-name" id="city-name" placeholder="City name" value={location} onChange={handleOnChange} />;
 }
