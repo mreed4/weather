dd78bbacbc64e6cfe19c80fd9e68e6e7c5380b6e
Matthew <78713317+mreed4@users.noreply.github.com>
2023-03-23 20:59:41 -0700

    Forecast complete

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "./AppContext";
+import Temperature from "./Temperature";
 
 export default function Forecast() {
   const [forecast, setForecast] = useState([]);
-  const { lat, lon, cityName } = useContext(AppContext);
+  const { lat, lon, cityName, convertDate } = useContext(AppContext);
 
   useEffect(() => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=5a35ef8f1d8400ee047265d3be990487`)
       .then((response) => response.json())
       .then((data) => {
+        // console.log(data);
         const noon = data.list.filter((item) => item.dt_txt.includes("12:00:00"));
         const weatherIds = noon.map((item) => item.weather[0].id);
         const temps = noon.map((item) => item.main.temp);
-        const dates = noon.map((item) => item.dt_txt);
+        const dates = noon.map((item) => convertDate(item.dt_txt));
         const forecastList = weatherIds.map((id, index) => {
           return {
             id,
@@ -21,7 +23,6 @@ export default function Forecast() {
           };
         });
         setForecast(forecastList);
-        console.log(forecast);
       })
       .catch((error) => console.error(error));
   }, [cityName]);
@@ -29,12 +30,12 @@ export default function Forecast() {
   return (
     forecast && (
       <ol>
-        {forecast.map((day) => {
+        {forecast.map((day, i) => {
           return (
-            <li>
-              <i className={`wi wi-owm-${day.id}`}></i>
+            <li key={i} className="forecast-day">
+              <i className={`wi wi-owm-${day.id} larger`}></i>
+              <Temperature temperature={day.temp} />
               <span>{day.date}</span>
-              <span>{day.temp}</span>
             </li>
           );
         })}
