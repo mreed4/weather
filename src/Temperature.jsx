4c656db469e6ab28e0c07bf3056dcbde5822f5a0
Matthew <78713317+mreed4@users.noreply.github.com>
2023-03-25 10:56:02 -0700

    Remove unused state/context

--- src/Temperature.jsx
+++ src/Temperature.jsx
@@ -3,7 +3,7 @@ import { AppContext } from "./AppContext";
 
 export default function Temperature(props) {
   // console.log(props);
-  const { temp, weather } = useContext(AppContext);
+  const { weather } = useContext(AppContext);
 
   return weather && <span>{Number(props.temperature, 10).toFixed(1) + "\u00B0"}</span>;
 }
